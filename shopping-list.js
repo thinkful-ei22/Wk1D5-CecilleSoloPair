54f36438db62a79c1087f808e51608e2c0155214
Cecille Salazar <cecillersalazar@gmail.com>
2018-06-15 05:38:37 -0700

    Fixed script tags

--- shopping-list.js
+++ shopping-list.js
@@ -1,23 +1,33 @@
-$(function(){
+//Creates a function called getGroceries - an interactive Shopping List
+
+$(const getGroceries = function() {
+
+//Event Listener for the form's submit button
 
 $('#js-shopping-list-form').submit(function(event) {
-
   event.preventDefault();
 
-  const listItem = $('.js-shopping-list-entry').val();
+//Contain the text box's input into a variable called item
 
-  $('.js-shopping-list-entry').val('');
+const item = $('.js-shopping-list-entry').val();
 
-  $('.shopping-list').append(
-      `<li>
-        <span class="shopping-item">${listItem}</span>
-        <div class="shopping-item-controls">
-          <button class="shopping-item-toggle">
-            <span class="button-label">check</span>
-          </button>
-          <button class="shopping-item-delete">
-            <span class="button-label">delete</span>
-          </button>
-        </div>
-      </li>`);
-  });
+//Sets the initial value of the text box input as empty
+
+$('.js-shopping-list-entry').val(' ');
+
+//With item defined, we append the shopping list class to account for new items
+
+$('.shopping-list').append(
+  `<li>
+    <span class="shopping-item">${item}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`);
+
+});
