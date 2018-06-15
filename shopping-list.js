37a7e586c3c3ce7193bb59d92d5c314425f033f2
Cecille Salazar <cecillersalazar@gmail.com>
2018-06-15 11:09:08 -0700

    Modified Files After Morning Session Foundation Code Established

--- shopping-list.js
+++ shopping-list.js
@@ -1,47 +1,119 @@
-//Creates an anonymous function with the goal of an interactive Shopping List
+'use strict';
 
-$(function() {
+const STORE = [
+  {name: 'apples', checked: false},
+  {name: 'oranges', checked: false},
+  {name: 'milk', checked: true},
+  {name: 'bread', checked: false}
+];
 
-//Event Listener for the form's submit button
 
-$('#js-shopping-list-form').submit(function(event) {
-  event.preventDefault();
+function generateItemElement(item, itemIndex, template) {
+  return `
+    <li class="js-item-index-element" data-item-index="${itemIndex}">
+      <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle js-item-toggle">
+            <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete js-item-delete">
+            <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`;
+}
 
-//Contain the text box's input into a variable called item
 
-const item = $('.js-shopping-list-entry').val();
+function generateShoppingItemsString(shoppingList) {
+  console.log('Generating shopping list element');
 
-//Sets the initial value of the text box input as empty
+  const items = shoppingList.map((item, index) => generateItemElement(item, index));
 
-$('.js-shopping-list-entry').val(' ');
+  return items.join('');
+}
 
-//With item defined, we append the shopping list class to account for new items
 
-$('.shopping-list').append(
-  `<li>
-    <span class="shopping-item">${item}</span>
-    <div class="shopping-item-controls">
-      <button class="shopping-item-toggle">
-        <span class="button-label">check</span>
-      </button>
-      <button class="shopping-item-delete">
-        <span class="button-label">delete</span>
-      </button>
-    </div>
-  </li>`);
+function renderShoppingList() {
+  // render the shopping list in the DOM
+  console.log('`renderShoppingList` ran');
+  const shoppingListItemsString = generateShoppingItemsString(STORE);
 
-});
+  // insert that HTML into the DOM
+  $('.js-shopping-list').html(shoppingListItemsString);
+}
 
-//Deletes an item from the list
 
-$('.shopping-list').on('click', '.shopping-item-delete', function(event) {
-    $(this).closest('li').remove();
+function addItemToShoppingList(itemName) {
+  console.log(`Adding "${itemName}" to shopping list`);
+  STORE.push({name: itemName, checked: false});
+}
+
+function handleNewItemSubmit() {
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    console.log('`handleNewItemSubmit` ran');
+    const newItemName = $('.js-shopping-list-entry').val();
+    $('.js-shopping-list-entry').val('');
+    addItemToShoppingList(newItemName);
+    renderShoppingList();
   });
+}
 
-//Checks an item from the list
+function toggleCheckedForListItem(itemIndex) {
+  console.log('Toggling checked property for item at index ' + itemIndex);
+  STORE[itemIndex].checked = !STORE[itemIndex].checked;
+}
 
-$('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+
+function getItemIndexFromElement(item) {
+  const itemIndexString = $(item)
+    .closest('.js-item-index-element')
+    .attr('data-item-index');
+  return parseInt(itemIndexString, 10);
+}
+
+function handleItemCheckClicked() {
+  $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
+    console.log('`handleItemCheckClicked` ran');
+    const itemIndex = getItemIndexFromElement(event.currentTarget);
+    toggleCheckedForListItem(itemIndex);
+    renderShoppingList();
   });
+}
 
-});
+function handleDeleteItemClicked() {
+  // this function will be responsible for when users want to delete a shopping list
+  // item
+
+  $('.js-shopping-list').on('click', `.js-item-delete`, event => {
+    console.log('`handleDeleteItemClicked` ran');
+    const itemIndex = getItemIndexFromElement(event.currentTarget);
+    deleteListItem(itemIndex);
+    renderShoppingList();
+  });
+}
+
+
+// name says it all. responsible for deleting a list item.
+function deleteListItem(itemIndex) {
+  console.log(`Deleting item at index  ${itemIndex} from shopping list`);
+
+  STORE.splice(itemIndex, 1);
+
+
+
+}
+
+// this function will be our callback when the page loads. it's responsible for
+// initially rendering the shopping list, and activating our individual functions
+// that handle new item submission and user clicks on the "check" and "delete" buttons
+// for individual shopping list items.
+function handleShoppingList() {
+  renderShoppingList();
+  handleNewItemSubmit();
+  handleItemCheckClicked();
+  handleDeleteItemClicked();
+}
+
+// when the page loads, call `handleShoppingList`
+$(handleShoppingList);
--- shopping-list.md
+++ shopping-list.md
@@ -1,18 +1,7 @@
-Requirements
-
 TO-DO's
 
-1. Enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button.
-2. Check and Uncheck items on the list by clicking the "Check" button.
-3. Permanently remove items from the list
+1. User can press a switch/checkbox to toggle between displaying all items or displaying only items that are unchecked.
+2. User can type in a search term and the displayed list will be filtered by item names only containing that search term.
+3. User can edit the title of an item.
 
-
-COMPLETE!
-1. You must use a CDN-hosted version of jQuery.
-2. Put your application code in a file called index.js and link to it in index.html.
-3. Be sure to put both script elements at the bottom of the <body> element.
-4. Do not alter index.html or main.css other than adding the links to the external JavaScript inside index.html. Write JavaScript code that works with the existing HTML and CSS to implement the required features.
-
-
-
-Hint: you may find it helpful to read up on and use the following jQuery methods: .submit(), preventDefault(), toggleClass(), and closest().
+COMPLETED
